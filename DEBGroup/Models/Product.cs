7caf36fa681db13662fd325a7c518b0a3549d7d4
Missetand <c.nielsen39@gmail.com>
2016-11-15 09:42:14 +0100

    Partial classes

---
+++ DEBGroup/Models/Product.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DEBGroup.EF
+{
+    public partial class Product
+    {
+        public Product(string productname)
+        {
+            this.ProductName = productname;
+        }
+    }
+}
---
+++ DEBGroup/Models/Room.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DEBGroup.EF
+{
+    public partial class Room
+    {
+        public Room(string roomname)
+        {
+            this.RoomName = roomname;
+        }
+    }
+}

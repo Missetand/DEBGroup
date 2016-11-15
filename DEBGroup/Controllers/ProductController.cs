84990b955799f4af3b36d5d8528f131e59c46ddd
Missetand <c.nielsen39@gmail.com>
2016-11-15 13:29:15 +0100

    Views og details ændring

---
+++ DEBGroup/Controllers/ProductController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DEBGroup.Controllers
+{
+    public class ProductController : Controller
+    {
+        EF.EAL39_DBEntities db = new EF.EAL39_DBEntities();
+        // GET: Product
+        public ActionResult Index()
+        {
+            
+            return View(db.Product.ToList());
+        }
+    }
+}
--- DEBGroup/EF/product.cs
+++ DEBGroup/EF/product.cs
@@ -22,6 +22,7 @@ namespace DEBGroup.EF
     
         public int ProductID { get; set; }
         public string ProductName { get; set; }
+        public string Details { get; set; }
     
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         public virtual ICollection<SCPconnection> SCPconnection { get; set; }

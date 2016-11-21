fe0e27c0a9d73c86ada62e3cf19121008f5cd883
Missetand <c.nielsen39@gmail.com>
2016-11-21 14:53:05 +0100

    Test der ikke virker

--- DEBGroup.Tests/U_SectorTest.cs
+++ DEBGroup.Tests/U_SectorTest.cs
@@ -10,13 +10,22 @@ namespace DEBGroup.Tests
         [TestMethod]
         public void ReadAllSectorsTest()
         {
-            DEBGroup.DatabaseFacade.IDatabaseFacadeSector dut = new FakeDatabaseFacade();
+            var db = new DEBGroup.DatabaseFacade.SectorDatabaseFacade(new FakeDatabaseFacade());
 
-            List<DEBGroup.EF.Sector> allSectors = dut.Read();
+            List<DEBGroup.EF.Sector> allSectors = db.Read();
 
             Assert.AreEqual(4, allSectors.Count);
             CollectionAssert.AllItemsAreUnique(allSectors);
-            
+
+        }
+        [TestMethod]
+        public void ReadAllSectorsTest_henten()
+        {
+            var db = new DEBGroup.DatabaseFacade.SectorDatabaseFacade(new FakeDatabaseFacade());
+
+            DEBGroup.EF.Sector ens = db.Read(2);
+
+            Assert.AreEqual("refhdk", ens.SectorName);
         }
     }
 }
---
+++ DEBGroup/EF_Helper/IEAL39_DBEntities.cs
@@ -0,0 +1,14 @@
+﻿using System.Data.Entity;
+
+namespace DEBGroup.EF
+{
+    public interface IEAL39_DBEntities
+    {
+        DbSet<Category> Category { get; set; }
+        DbSet<CustomerInfo> CustomerInfo { get; set; }
+        DbSet<Product> Product { get; set; }
+        DbSet<Room> Room { get; set; }
+        DbSet<SCPconnection> SCPconnection { get; set; }
+        DbSet<Sector> Sector { get; set; }
+    }
+}

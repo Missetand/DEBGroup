e1d15006327423d587374e961d346ace1eb7ce46
Missetand <c.nielsen39@gmail.com>
2016-11-21 10:09:29 +0100

    Break

--- DEBGroup/Controllers/CategoryController.cs
+++ DEBGroup/Controllers/CategoryController.cs
@@ -10,18 +10,21 @@ namespace DEBGroup.Controllers
     {
         EF.EAL39_DBEntities db = new EF.EAL39_DBEntities();
         // GET: Category
-        public ActionResult Index()
+        public ActionResult Index(int id)
         {
             var m = new Models.Category.Index();
 
             m.DisplayName = "Categories";
+            
 
-            foreach (var item in db.Category.OrderBy(c => c.CategoryName))
-            {
-                var mc = new Models.Category.ModelCategory();
-                mc.CategoryName = item.CategoryName;
-                mc.CategoryID = item.CategoryID;
-                m.AllCategories.Add(mc);
+                foreach (var item in db.Category.OrderBy(c => c.CategoryName))
+                {
+                    var mc = new Models.Category.ModelCategory();
+                    mc.CategoryName = item.CategoryName;
+                    mc.CategoryID = item.CategoryID;
+                    m.AllCategories.Add(mc);
+                    if (item.CategoryID == id)
+                    break;
             }
             return View(m);
         }
--- DEBGroup/Controllers/ProductController.cs
+++ DEBGroup/Controllers/ProductController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Data.Entity;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
 
@@ -11,21 +12,26 @@ namespace DEBGroup.Controllers
     {
         EF.EAL39_DBEntities db = new EF.EAL39_DBEntities();
         // GET: Product
-        public ActionResult Index()
+        public ActionResult Index(int id)
         {
             var m = new Models.Product.Index();
+            
+            //m.DisplayName = "Produkter";
+            //var scp = db.SCPconnection.FirstOrDefault(s => s.CategoryID == id);
+            //if (id == scp.ProductID)
+                {
 
-            m.DisplayName = "Produkter";
-
-            foreach (var item in db.Product.OrderBy(p => p.ProductName))
-            {
-                var mp = new Models.Product.ModelProduct();
-                mp.SectorName = item.ProductName;
-                mp.ProductID = item.ProductID;
-                mp.DetailsUrl = item.Details;
-                m.AllProducts.Add(mp);
+                foreach (var item in db.Product.OrderBy(p => p.ProductName))
+                {
+                    var mp = new Models.Product.ModelProduct();
+                    mp.SectorName = item.ProductName;
+                    mp.ProductID = item.ProductID;
+                    mp.DetailsUrl = item.Details;
+                    m.AllProducts.Add(mp);  
+                    if (item.ProductID == id)
+                    break;
+                }
             }
-
             return View(m);
         }
 
@@ -39,6 +45,32 @@ namespace DEBGroup.Controllers
             return View(product);
         }
 
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
 
+            var product = db.Product.FirstOrDefault(p => p.ProductID == id);
+
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+            return View(product);
+        }
+
+        [HttpPost, ActionName ("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id)
+        {
+            var product = db.Product.FirstOrDefault(p => p.ProductID == id);
+            var scp = db.SCPconnection.FirstOrDefault(s => s.ProductID == id);
+            db.SCPconnection.Remove(scp);
+            db.Product.Remove(product);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
     }
 }

a3fad22e3f1a4f1ec82c9b7a0fa7f021bd517ad9
Missetand <c.nielsen39@gmail.com>
2016-11-21 11:56:24 +0100

    Category Controller

--- DEBGroup/Controllers/CategoryController.cs
+++ DEBGroup/Controllers/CategoryController.cs
@@ -14,18 +14,20 @@ namespace DEBGroup.Controllers
         {
             var m = new Models.Category.Index();
 
-            m.DisplayName = "Categories";
-            
+            // select * from sector where sectorid = 1
+            m.DisplayName = db.Sector.FirstOrDefault(p => p.SectorID == id).SectorName;
 
-                foreach (var item in db.Category.OrderBy(c => c.CategoryName))
-                {
-                    var mc = new Models.Category.ModelCategory();
-                    mc.CategoryName = item.CategoryName;
-                    mc.CategoryID = item.CategoryID;
-                    m.AllCategories.Add(mc);
-                    if (item.CategoryID == id)
-                    break;
+            // select distinct CategoryName, s.CategoryID from SCPconnection s, Category g where s.SectorID = 1 and s.CategoryID = g.CategoryID
+            var aallCategoryID = db.SCPconnection.Where(p => p.SectorID == id).Select(p => p.CategoryID).Distinct();
+            foreach (var CategoryID in aallCategoryID)
+            {
+                var mc = new Models.Category.ModelCategory();
+                var c = db.Category.FirstOrDefault(p => p.CategoryID == CategoryID);
+
+                mc.CategoryName = c.CategoryName;
+                m.AllCategories.Add(mc);
             }
+
             return View(m);
         }
         public ActionResult Edit(int id)
